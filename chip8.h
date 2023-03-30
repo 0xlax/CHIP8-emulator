7a979e428279a9bcb20854057ce5c525ffb8e57f
0xlax <harshu8310@gmail.com>
2023-03-30 20:58:23 +0530

    skeleton

---
+++ chip8.h
@@ -0,0 +1,14 @@
+
+
+unsigned short opcode;
+unsigned char memory[4096];
+unsigned char V[16];
+
+unsigned short I;
+unsigned short pc;
+unsigned char graphics[64*32];
+unsigned char delay;
+unsigned char sound;
+unsigned char sp;
+unsigned char stack[16];
+
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <chip8.h>
+
+int main(int argc, char **argv)
+{
+    // 1) Read file
+    
+
+
+    // 2) Initialise Graphics (SDL)
+
+
+    // 3) Initialise game and load ROM
+
+
+    // 4) Emulation loop
+}
